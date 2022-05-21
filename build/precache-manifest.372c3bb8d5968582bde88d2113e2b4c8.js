7dead81ed88c3c2b08e81e191447372ac76a2e33
voxom <voxom13@gmail.com>
2022-05-21 13:26:35 -0500

    update

---
+++ build/precache-manifest.372c3bb8d5968582bde88d2113e2b4c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abfd8728ec3aa82380f6c3e418438dcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe28bc3d08646de78ee3",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "d7ebdfa652610e34ac92",
+    "url": "/static/js/2.17e8b321.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/static/js/2.17e8b321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe28bc3d08646de78ee3",
+    "url": "/static/js/main.5d912a29.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  },
+  {
+    "revision": "a30a2978d914df8472934d8f3bb835e5",
+    "url": "/static/media/sample.a30a2978.mp4"
+  }
+]);
